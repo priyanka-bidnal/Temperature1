468bc563ed388932caaa0f440b7b7b1a63b08ec2
priyanka-bidnal <bidnalpriyanka@gmail.com>
2025-11-27 14:56:40 +0530

    Update tem.py

--- tem.py
+++ tem.py
@@ -3,9 +3,9 @@ if len(sys.argv)==1:
   temp=sys.argv[0]
 else:
   temp=33
-if(temp<=15):
+if temp<=15:
   temp1="Cold"
-elif(15<=temp>=30):
+elif 15<=temp>=30:
   temp1="Normal"
 else:
   temp1="Hot"
